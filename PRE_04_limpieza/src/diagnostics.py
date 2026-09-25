1619bca7b0cc72312195d34dd672f376a7bbcf40
oocampoa-gif <oocampoa@unal.edu.co>
2026-09-25 15:07:22 -0500

    PRE_04

--- PRE_04_limpieza/src/diagnostics.py
+++ PRE_04_limpieza/src/diagnostics.py
@@ -5,9 +5,11 @@ OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
 
 def main():
     df = pd.read_csv(OUTPUT_FILE)
-    series = df["purchase_date"]
 
-    # series = series[series.str.contains(r"-\d{2}$", regex=True)]
+    series = df["discount"]
+
+    #series = series.astype(str)
+    #series = series[series.str.startswith("%")]
 
     series = series.sort_values()
     series = series.drop_duplicates()
@@ -18,5 +20,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
+
+
+
